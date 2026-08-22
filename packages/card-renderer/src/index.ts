e1a6f1d17caa454eaec8724ddaf22033c6fe3cc5
Gabriel Aureo <gabrielaureo@gmail.com>
2026-08-22 09:37:47 -0300

    Export subject cache controls

--- packages/card-renderer/src/index.ts
+++ packages/card-renderer/src/index.ts
@@ -1,5 +1,7 @@
 export { CardRenderer } from './CardRenderer';
 export { HoloEffectPreview } from './HoloEffectPreview';
+export { clearSubjectCache, getSubjectCacheStats } from './subject-cache';
 export type { ArtworkMetrics, ArtworkPlacement, CardRendererProps, CardRendererStatus } from './CardRenderer';
 export type { HoloEffectPreviewProps } from './HoloEffectPreview';
+export type { SubjectCacheStats } from './subject-cache';
 export type { CardDefinition } from '@holo/card-schema';
